4dfa15540454210c640c5019aaba87e2be36729e
abidroid <abidroid1@gmail.com>
2021-07-23 17:20:59 +0500

    bottom container Text size

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -92,7 +92,7 @@ class LoginScreen extends StatelessWidget {
                         'Don\'t have account ? Sign up',
                         style: TextStyle(
                           color: Colors.white,
-                          fontWeight: FontWeight.w600,
+                          fontWeight: FontWeight.w500,
                           fontSize: 20.0,
                         ),
                       ),
