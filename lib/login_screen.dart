143631bebb57b15f4981e461020e4f96a269ab67
abidroid <abidroid1@gmail.com>
2021-07-23 17:17:51 +0500

    Initial commit

---
+++ lib/login_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+import 'CurvedClipper.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              ClipPath(
+                clipper: CurvedClipper(),
+                child: Image(
+                  image: AssetImage(
+                    'assets/images/nature2.jpeg',
+                  ),
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                'TODO',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Username',
+                      prefixIcon: Icon(Icons.account_box),
+                      contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                      fillColor: Colors.white,
+                      filled: true),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: 'Password',
+                      prefixIcon: Icon(Icons.lock),
+                      contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                      fillColor: Colors.white,
+                      filled: true),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: GestureDetector(
+                    onTap: () {
+                      print('Sign up');
+                    },
+                    child: Container(
+                      height: 100,
+                      alignment: Alignment.center,
+                      color: Theme.of(context).primaryColor,
+                      child: Text(
+                        'Don\'t have account ? Sign up',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
